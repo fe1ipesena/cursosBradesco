94888f470e498127bb60c6f25426f14df09e473b
felipe <felipecsena877@gmail.com>
2023-10-01 02:36:27 -0300

    concluido

---
+++ criandoSiteSimples/js/script.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const switcher = document.querySelector('.btn')
+
+switcher.addEventListener('click', function(){
+document.body.classList.toggle('dark-theme')
+
+var className = document.body.className
+if(className == 'light-theme'){
+    this.textContent = 'Escuro'
+    console.log(`current class name: ${className}`)
+}
+else{
+    this.textContent = 'Claro'
+    console.log(`current class name: ${className}`)
+}
+
+})
+
+
